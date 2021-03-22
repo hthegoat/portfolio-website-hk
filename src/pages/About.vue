348130fd57f6ff1a15448bd6271264ac6f24b58c
hthegoat <harrisonkug@protonmail.com>
2021-03-22 14:30:55 -0400

    done

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -2,15 +2,15 @@
   <Layout>
     <section class="text-gray-700 body-font">
             <div class="container flex flex-col items-center px-5 py-16 mx-auto lg:px-20 lg:py-24 md:flex-row">
-                <div class="w-5/6 mb-10 lg:max-w-lg lg:w-full md:w-1/2 md:mb-0">
+                <div class="w-5/6 mb-10 lg:max-w-lg lg:w-full md:w-1/2 md:mb-0 sm:w-full">
                     <img class="object-cover object-center rounded" alt="hero"
                         src="https://scontent-lga3-2.xx.fbcdn.net/v/t1.0-9/91768250_10157570766824092_2733691132796993536_o.jpg?_nc_cat=108&ccb=3&_nc_sid=174925&_nc_ohc=5UhxGINZGrcAX9tyckJ&_nc_ht=scontent-lga3-2.xx&oh=65c5170086def16c797f83382fd94d36&oe=605EBAEC">
                 </div>
                 <div
                     class="flex flex-col items-center text-center lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 md:items-start md:text-left">
-                <h1 class="mb-8 text-2xl font-bold tracking-tighter text-center text-blue-700 lg:text-left lg:text-2xl title-font">
+                <h1 class="mb-8 text-2xl font-bold tracking-tighter text-center text-blue-700 lg:text-left lg:text-2xl sm:text-3xl title-font">
                     I consider myself a generalist at the end of the day.</h1>
-                <p class="mb-8 text-base leading-relaxed text-center text-gray-700 lg:text-left lg:text-1xl">I am a freelance marketing professional with experience in multiple industries. I work with clients to develop actionable marketing campaigns that convert and drive sales.
+                <p class="mb-8 text-base leading-relaxed text-center text-gray-700 lg:text-left lg:text-1xl">I am a freelance marketing professional with experience in multiple industries. I work with clients to develop actionable marketing campaigns that convert and drive sales. In addition to marketing I have a variety of other interests and side projects. 
 
 </p>
                 <div class="flex flex-wrap w-full mt-2 -mx-4 text-center lg:text-left ">
@@ -31,9 +31,9 @@
                 </div>
             </div>
             </div>
-             <div class="text-gray-700 body-font container px-8 pt-24 mx-auto lg:px-4">
+             <div class="text-gray-700 body-font container px-8 pt-24 mx-auto lg:px-4 sm:px-0">
                 <div class="flex flex-wrap text-left">
-                    <div class="px-8 py-6 lg:w-1/3 md:w-full">
+                    <div class="px-8 py-6 lg:w-1/3 md:w-full sm:px-0">
                         <div class="p-6 rounded-md bg-blueGray-200">
                         <img class="object-cover object-center w-full h-60 mb-6 rounded"
                             src="https://i.ibb.co/Vv00wnz/photo-2021-02-28-13-49-13.jpg" alt="content">
@@ -43,7 +43,7 @@
                           
                         </div>
                     </div>
-                    <div class="px-8 py-6 lg:w-1/3 md:w-full">
+                    <div class="px-8 py-6 lg:w-1/3 md:w-full sm:px-0">
                         <div class="p-6 rounded-md bg-blueGray-200">
                             <img class="object-cover object-center w-full h-60 mb-6 rounded"
                                 src="https://images.unsplash.com/photo-1576520804567-b88e5dade12d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1451&q=80" alt="content">
@@ -53,7 +53,7 @@
                             
                         </div>
                     </div>
-                    <div class="px-8 py-6 lg:w-1/3 md:w-full">
+                    <div class="px-8 py-6 lg:w-1/3 md:w-full sm:px-0">
                         <div class="p-6 rounded-md bg-blueGray-200">
                             <img class="object-cover object-center w-full h-60 mb-6 rounded"
                                 src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="content">
@@ -72,9 +72,9 @@
                     </div>
                 </div>
             </div>
-            <div class="text-gray-700 body-font container px-8 pt-24 mx-auto lg:px-4">
+            <div class="text-gray-700 body-font container px-8 pt-24 mx-auto lg:px-4 sm:px-0">
                 <div class="flex flex-wrap text-left">
-                    <div class="px-8 py-6 lg:w-1/3 md:w-full">
+                    <div class="px-8 py-6 lg:w-1/3 md:w-full sm:px-0">
                         <div class="p-6 rounded-md bg-blueGray-200">
                         <img class="object-cover object-center w-full h-60 mb-6 rounded"
                             src="https://images.unsplash.com/photo-1579829366248-204fe8413f31?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" alt="content">
@@ -95,7 +95,7 @@
                         </a>
                         </div>
                     </div>
-                    <div class="px-8 py-6 lg:w-1/3 md:w-full">
+                    <div class="px-8 py-6 lg:w-1/3 md:w-full sm:px-0">
                         <div class="p-6 rounded-md bg-blueGray-200">
                             <img class="object-cover object-center w-full h-60 mb-6 rounded"
                                 src="https://images.unsplash.com/photo-1584755209366-71ade5517ee0?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1324&q=80" alt="content">
@@ -107,7 +107,7 @@
                             
                         </div>
                     </div>
-                    <div class="px-8 py-6 lg:w-1/3 md:w-full">
+                    <div class="px-8 py-6 lg:w-1/3 md:w-full sm:px-0">
                         <div class="p-6 rounded-md bg-blueGray-200">
                         <a href="https://jpeg.chat" target="_blank">
                             <img class="object-cover object-center w-full h-60 mb-6 rounded"
@@ -130,11 +130,11 @@
                 </div>
             </div>
         </section>
-         <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32">
+         <div class="container px-5 py-2 mx-auto lg:pt-24 lg:px-32 sm:px-0 sm:pb-0 sm:max-w-full">
          <h2 class="mb-3 text-lg font-semibold text-gray-700 lg:text-2xl title-font"> Everything Else
                             </h2>
-            <div class="flex flex-wrap -m-1 md:-m-2">
-                <div class="flex flex-wrap w-1/2">
+            <div class="flex flex-wrap -m-1 md:-m-2 sm:flex-col">
+                <div class="flex flex-wrap w-1/2 sm:w-full">
                     <div class="w-1/2 p-1 md:p-2">
                         <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
                             src="https://i.ibb.co/Hp6Kmrb/124026684-370287604237680-5585876580566453797-n.jpg">
@@ -148,7 +148,7 @@
                             src="https://scontent-lga3-2.xx.fbcdn.net/v/t1.0-9/131066857_10159140748816085_6781653498238789988_o.jpg?_nc_cat=104&ccb=3&_nc_sid=174925&_nc_ohc=zaGMM2ESpjIAX-d522e&_nc_ht=scontent-lga3-2.xx&oh=0f75516e60dd817e2cd85410bc5e8f7f&oe=60648DBB">
                     </div>
                 </div>
-                <div class="flex flex-wrap w-1/2">
+                <div class="flex flex-wrap w-1/2 sm:w-full">
                     <div class="w-full p-1 md:p-2">
                         <img alt="gallery" class="block object-cover object-center w-full h-full rounded-lg"
                             src="https://scontent-lga3-2.xx.fbcdn.net/v/t1.0-9/40991595_10214104793626712_5411711178676109312_n.jpg?_nc_cat=106&ccb=3&_nc_sid=8bfeb9&_nc_ohc=Zk7jGnoJ1e0AX9qAAwV&_nc_ht=scontent-lga3-2.xx&oh=ee2a9614b359af2c0218de4fc751cf2e&oe=6061DE46">
